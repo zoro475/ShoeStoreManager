8c737fe6d8f5edbefd48a4b537768f4a392ecde0
ninh28 <ninhquachhai0@gmail.com>
2025-07-16 07:54:56 +0700

    Add files via upload
    code duan 1

---
+++ duan1code/src/model/Category.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class Category {
+private int id;
+    private String name;
+
+    public Category() {}
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
